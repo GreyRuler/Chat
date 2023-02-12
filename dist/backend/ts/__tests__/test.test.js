f760ae3d65a0693b272d060ff19c6e4d9c9059d5
GreyRuler <daniil_vasilenko757@mail.ru>
2023-02-12 16:51:02 +0300

    fix

--- dist/backend/ts/__tests__/test.test.js
+++
@@ -1,5 +0,0 @@
-import { expect, test } from '@jest/globals';
-test('testName', function () {
-    expect('test').toBe('test');
-});
-//# sourceMappingURL=test.test.js.map
--- dist/backend/ts/routes/index.js
+++
@@ -1,8 +0,0 @@
-import combineRouters from 'koa-combine-routers';
-import users from './users';
-import sse from './sse';
-var router = combineRouters(
-// @ts-ignore
-users, sse);
-export default router;
-//# sourceMappingURL=index.js.map
